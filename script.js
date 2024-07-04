ea24a08c9fde5e866c30f49c7594b7c15a4e7d75
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-04 09:14:24 +0500

    email hide is incorrect

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var email = prompt("Enter the e-mail");
 
 if (email.length >= 3) {
-    email.padEnd(3 , "*");
+    email.padEnd(email.length + (email.length < 3 ? 3  - email.length : 0), "*");
     alert(email);
 } else {
     alert("it is invalid e-mail");
