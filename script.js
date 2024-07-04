8cb7281a87b823e465d5c988b14106d50d6d12e2
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-04 09:22:40 +0500

    next turn

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 var email = prompt("Enter the e-mail");
 
+function detect(email) {
+    var atIndex = email.indexOf("@");
+
+    
+}
+
 if (email.length >= 3) {
     email.padEnd(email.length + (email.length < 3 ? 3  - email.length : 0), "*");
     alert(email);
