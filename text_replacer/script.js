021bf5c9284a7b15801af954827de493953e16cb
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-05 17:51:58 +0500

    without regexp

--- text_replacer/script.js
+++ text_replacer/script.js
@@ -1,3 +1,11 @@
 var text = prompt("Enter the text");
 var find = prompt("Find the word");
 var rep = prompt("Word for replace");
+
+if (text && find) {
+    var startindex = 0;
+    var newtext = 0;
+    
+} else {
+    
+}
