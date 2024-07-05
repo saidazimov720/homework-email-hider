b07741b091887ae097c03005e896f6f9684082bc
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-05 18:03:21 +0500

    last fix

--- text_replacer/script.js
+++ text_replacer/script.js
@@ -3,7 +3,19 @@ var find = prompt("Find the word");
 var rep = prompt("Word to replace with");
 
 if (text && find) {
-    
+    var startIndex = 0;
+    var newText = '';
+
+    for (var i = 0; i < text.length; i++) {
+        if (text.substring(i, i + find.length) === find) {
+            newText += text.substring(startIndex, i) + rep;
+            i += find.length - 1;
+            startIndex = i + 1;
+        }
+    }
+
+    newText += text.substring(startIndex);
+    alert("Updated text: " + newText);
 } else {
     alert("Please enter both the text and the word to find.");
 }
